4641d07f0241da561c455fbf3262e4e8038f718a
Dejan Prole <tamodaleko@users.noreply.github.com>
2014-12-24 00:18:21 +0100

    Update example_model.php

--- application/models/example_model.php
+++ application/models/example_model.php
@@ -5,8 +5,9 @@ class Example_model extends Model {
 	public function getSomething($id)
 	{
 		$id = $this->escapeString($id);
-		$result = $this->query('SELECT * FROM something WHERE id="'. $id .'"');
+		$result = $this->query('SELECT * FROM users WHERE id='. $id);
 		return $result;
+		$this->closeConnection();
 	}
 
 }
